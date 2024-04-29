db7a158ecbdf4c2e6c108ac7dcfcfd107404e313
JuanMoreno0817 <juanelmorenito13@gmail.com>
2024-04-29 17:28:29 -0500

    Add class Person and add method in DataBaseContext

--- PetCareWebAPI/DAL/DataBaseContext.cs
+++ PetCareWebAPI/DAL/DataBaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PetCareWebAPI.DAL.Entities;
 using System.Diagnostics.Metrics;
 
 
@@ -7,5 +8,13 @@ namespace PetCareWebAPI.DAL
     public class DataBaseContext : DbContext
     {
         public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Person>().HasIndex(p => p.Identification).IsUnique();
+        }
     }
 }
---
+++ PetCareWebAPI/DAL/Entities/Person.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetCareWebAPI.DAL.Entities
+{
+    public class Person
+    {
+        [Key]
+        [Display(Name = "Documento")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int Identification { get; set; }
+
+        [Display(Name = "Nombres")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Name { get; set; }
+
+        [Display(Name = "Apellidos")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Lastname { get; set; }
+
+        [Display(Name = "Celular")]
+        [MaxLength(10, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Cellphone { get; set; }
+
+        [Display(Name = "Dirección")]
+        [MaxLength(30, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string Address { get; set; }
+
+        [Display(Name = "Correo Electronico")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Email { get; set; }
+
+        [Display(Name = "Contraseña")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Password { get; set; }
+
+        [Display(Name = "Fecha Nacimiento")]
+        public DateTime Borndate { get; set; }
+    }
+}
