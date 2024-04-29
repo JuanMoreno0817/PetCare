0f480c3cabd059989b6ee372b433dc9cc765e5ef
JuanMoreno0817 <juanelmorenito13@gmail.com>
2024-04-29 17:16:36 -0500

    Add DataBaseContext and Configurate the Program.cs

---
+++ PetCareWebAPI/DAL/DataBaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+
+
+namespace PetCareWebAPI.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
+    }
+}
