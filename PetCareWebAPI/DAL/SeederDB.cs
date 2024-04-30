77414d25ada0034243a50ba0cd407b9723980b92
JuanMoreno0817 <juanelmorenito13@gmail.com>
2024-04-29 19:17:55 -0500

    Add SeederDB class

---
+++ PetCareWebAPI/DAL/SeederDB.cs
@@ -0,0 +1,19 @@
+﻿namespace PetCareWebAPI.DAL
+{
+    public class SeederDB
+    {
+        private readonly DataBaseContext _context;
+
+        public SeederDB(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeederAsync() 
+        {
+            await _context.Database.EnsureCreatedAsync();
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
