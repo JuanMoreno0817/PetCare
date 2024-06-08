650969b3e2f4a798787e48801edeb788b3ff8cc7
WillianOrozco <elicardenas1986@gmail.com>
2024-06-07 20:12:36 -0500

    Post, Update, Delete in Vet Component

--- PetCareFront/PetCare/src/app/veterinario/veterinario.services.ts
+++ PetCareFront/PetCare/src/app/veterinario/veterinario.services.ts
@@ -11,16 +11,40 @@ export class VeterinarioService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getAllMedicalRecords():Observable<MedicalRecord[]>{
+  getAllMedicalRecords(): Observable<MedicalRecord[]> {
     const token = localStorage.getItem('Token'); // Obtén el token del localStorage
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    return this.httpClient.get<MedicalRecord[]>("https://localhost:7056/api/MedicalRecord/GetMedicalRecords", {headers});
+    return this.httpClient.get<MedicalRecord[]>("https://localhost:7056/api/MedicalRecord/GetMedicalRecords", { headers });
   }
-  
+
   getInfo(): Observable<Vet> {
     const token = localStorage.getItem('Token'); // Obtén el token del localStorage
     const idUser = localStorage.getItem('idUser');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     return this.httpClient.get<Vet>(`https://localhost:7056/api/Vet/GetVet/${idUser}`, { headers });
-}
+  }
+
+  getMedicalRecord(id: string): Observable<MedicalRecord> {
+    const token = localStorage.getItem('Token'); // Obtén el token del localStorage
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.httpClient.get<MedicalRecord>(`https://localhost:7056/api/MedicalRecord/GetMedicalRecord/${id}`, { headers });
+  }
+
+  postMedicalRecord(medicalRecord: MedicalRecord):Observable<MedicalRecord>{
+    const token = localStorage.getItem('Token'); // Obtén el token del localStorage
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.httpClient.post<MedicalRecord>(`https://localhost:7056/api/MedicalRecord/CreateMedicalRecord`, medicalRecord, { headers });
+  }
+
+  putMedicalRecord(medicalRecord: MedicalRecord):Observable<MedicalRecord>{
+    const token = localStorage.getItem('Token'); // Obtén el token del localStorage
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.httpClient.put<MedicalRecord>(`https://localhost:7056/api/MedicalRecord/EditMedicalRecord`, medicalRecord, { headers });
+  }
+
+  deleteMedicalRecord(id: string):Observable<MedicalRecord>{
+    const token = localStorage.getItem('Token'); // Obtén el token del localStorage
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.httpClient.delete<MedicalRecord>(`https://localhost:7056/api/MedicalRecord/DeleteMedicalRecord/${id}`, { headers });
+  }
 }
